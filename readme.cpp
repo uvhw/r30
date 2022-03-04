3e26f7d51bcb0fb1b43eff96a3369204b13e8c3a
uvhw <uvhwuvhw@gmail.com>
2022-03-04 23:07:59 +0700

    Create readme.cpp

---
+++ readme.cpp
@@ -0,0 +1,108 @@
+#1 Operational Pools
+
+The following mining pools are still running and paying out their users:
+Name	Location	Size[1]	Merged Mining[2]	Reward Type	Transaction fees	PPS Fee	Other Fee	Stm.png	GBT	Launched	Variance	Forum	Website
+AntPool	China	Large	No	PPLNS & PPS	kept by pool	2.5%	0%	Yes	No	 ?	 ?	link	link
+BCMonster.com	United StatesEuropeChina	Small	No	PPLNS	shared		0.5%	Yes	No	2016-01-13	Dynamic	link	link
+BitMinter	United StatesEurope	Small	NMC	PPLNSG	shared		1%	Yes	No	2011-06-26	User[3]/Dynamic	link	link
+BTC.com	ChinaUnited StatesEurope	Medium	NMC	FPPS	shared	0%	4%	Yes	No	2016-09-13	User	link	link
+BTCC Pool	China, Japan	Large	NMC	PPS	kept by pool	2.0%	0%	Yes	Yes	2014-10-21	Dynamic	 ?	link
+BW Mining	China	Medium	 ?	PPLNS & PPS	 ?	 ?	 ?	Yes		 ?	 ?	 ?	link
+F2Pool	United StatesEuropeChina	Large	LTC, NMC, SYS, EMC	PPS+	shared	2.5%	0%	Yes	No	2013-05-05	Dynamic	link	link
+Golden Nonce Pool	United StatesEurope	Small	No	DGM	kept by pool		0%	Yes		2018-03-27	Dynamic	link	link
+KanoPool	United StatesSingaporeGermanyJapanNetherlands	Medium	No	PPLNSG	shared		0.9%	Yes	No	2014-09-20	User[3]/Dynamic 18SPM	link	link
+Merge Mining Pool	United States	Small	NMC, IXC, Devcoin	DGM	shared		1.5%	Yes	No	2012-01-08	User[3]	link	link
+P2Pool	Global (p2p)	Small	Merged mining can be done on a "solo mining" basis [4]	PPLNS	shared		0%	Yes	No	2011-06-17	User[3]	link	
+Poolin	Global	Large	NMC DOGE VCASH	FPPS	shared	2.5%	0%	Yes	Yes	2017-10-01	Dynamic	link	link
+SBICrypto Pool	Global	Medium	No	FPPS	shared	0%	0%	Yes	Yes	2020-11-03	User[3]/Dynamic		link
+Slush Pool	Global	Medium	NMC	Score	shared		2%	Yes	No	2010-11-27	User[3]	link	link
+Luxor	United States	Medium	BTC, DASH, ZEC, ZEN, DCR, SC, XMR, LBC, ARRR & KMD	FPPS & PPS	shared	2%	0%	Yes		2018-01-01	VarDiff		link
+# Defunct Pools
+The following pools were once operational but have since shut down. They are listed for historical purposes only.
+Name	Location	Size[1]	Merged Mining[2]	Reward Type	Transaction fees	PPS Fee	Other Fee	Stm.png	GBT	Launched	Variance	Forum	Website
+BitcoinAffiliateNetwork	United StatesEuropeChinaNetherlandsAustralia	 ?	NMC, DOGE	 ?	kept by pool	 ?	 ?	Yes		2014-07-15	User/Dynamic	link	link
+BTCDig	United States	Small	No	DGM	kept by pool		0%	Yes		2013-07-04	User[3]/Dynamic 20SPM	link	link
+btcmp.com	Germany	Small	No	PPS	kept by pool	4%		Yes		2011-06-28	Diff 1		link
+btcZPool.com	United States	Large	BitCoinZ	PPLNS	shared	1%	0%	Yes		2017-06-25	VarDiff		link
+Eligius	United States	Small	NMC	CPPSRB	shared		0%	Yes	Yes	2011-04-27	Dynamic: 32 shares/m	link	link
+EMCD Pool	CIS, EU, KZ, IR, China	Medium	DOGE	FPPS+	shared	1%	0%	Yes	No	2018-01-26	Dynamic		link
+GHash.IO	Netherlands	Small	NMC, IXC, Devcoin	PPLNS	shared		0%	Yes	No	2013-07-01	User[3]	link	link
+Give Me COINS	United StatesEurope	Small	NMC	PPLNS	shared		0%	Yes	Yes	2013-08-12	Dynamic	link	link
+Jonny Bravo's Mining Emporium	United StatesEurope	Small	No	PPLNS	shared		0.5%	Yes	No	2015-11-19	Dynamic	link	link
+kmdPool.org	United States	Large	Komodo	PPLNS	shared	1%	0%	Yes		2017-11-25	VarDiff		link
+MergeMining	Global	Small	CRW, DVC, HUC, I0C, IXC, XMY/MYR, NMC, SYS, UNO, TRC, ARG, EMC	PPLNS	shared		1%	Yes	No	2016-12-01	User[3]		link
+Multipool	United StatesEurope	Small	NMC	Score	shared		1.5%	Yes	No	2012-03-15	User	link	link
+ZenPool.org	United States	Large	ZenCash	PPLNS	shared	1%	0%	Yes		2017-10-25	VarDiff		link
+
+
+# SPV Mining / Old Bitcoin Core
+The following pools are known or strongly suspected to be mining on top of blocks before fully validating them with Bitcoin Core 0.9.5 or later. Miners doing this have already lost over $50,000 USD during the 4 July 2015 fork and have created a situation where small numbers of confirmations are much less useful than they normally are.
+BTC Nuggets
+F2Pool[5]
+AntPool[5]
+The following pools are believed to be currently fully validating blocks with Bitcoin Core 0.9.5 or later (0.10.2 or later recommended due to DoS vulnerabilities):
+BitMinter
+BTC China (described as SPV mining[5], but they're performing effective valiation)
+BTC Public Mining Pool
+CKPool
+Eligius
+Golden Nonce Pool
+P2Pool
+Slush Pool
+BCMonster.com
+Jonny Bravo's Mining Emporium
+# References
+↑ Jump up to: 1.0 1.1 Note that pool hashrate is largely irrelevant but can be seen as a popularity measurement. It is a theoretical security issue if one pool gains above 50% of the total computational power of the network, thus consider joining a pool based on other metrics. The pool's total hash rate is very dynamic on most pools. Over time, as the network grows, so does most pool's hash rates. The displayed values are the pool's relative sizes based on the network: Small: less than 2%, Medium: 2%-10% Large: greater than 10% of the network.
+↑ Jump up to: 2.0 2.1 Merged mining allows miners to mine on multiple block chains at the same time with the same hashing.
+↑ Jump up to: 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8 The difficulty of the shares can be changed by the user.
+Jump up ↑ Merged mining can be done on a "solo mining" basis (payouts in the merged chain are not pooled).
+↑ Jump up to: 5.0 5.1 5.2 Intention to continue SPV mining, Wang Chun, 4 July 2015
+# See also
+Pooled mining
+Pool Distribution Summary
+Bitcoin Mining
+Video: What is Bitcoin Mining
+Bitcoin Mining Pools
+Bitcoin Mining Pools Comparison
+Mining pools
+Background • Comparison
+Major	
+Bitfury • BTC Guild • BW • Eligius • F2Pool • KanoPool • P2Pool • PolMine • Slush's pool • Tangpool • Poolin • SBICrypto
+Bitcoin-proof-of-work.png
+Active	
+50BTC • AntPool • Bitalo • Bitcoin Affiliate Network • Bitfinex • BitMinter • BTCDig • btcmp • CloudHashing • EclipseMC • Give Me COINS • Golden Nonce Pool • mmpool • MinerGate • Multipool • Halleychina.com • Ozco.in • Triplemining
+Fledgling	
+Cucumber Pool
+Solo	
+BitSolo • solo.ckpool
+Defunct	
+21bitcoin • ABCPool.co • ArsBitcoin • Betcoin.co Pool • Bitalo • BitClockers • Bitcoin-Station • BitcoinMining.Co • BitcoinPool.com • Bitcoins.lc • Bitp.it • BitPenny • BTC Canada • BTCMine • BtcPool.ru • BTCPoolman • Coinium • Coinotron • Compute 4 Cash • Continuum • DeepBit • Galaxy • GHash.IO • HHTT • KnCMiner • MegaBigPower • MineCo.in • Mining Team Reddit • Nexious • NoFeeMining • PolishPool • pool.itzod.ru • rfcpool • Simplecoin.us • Swepool • UnitedMiners • x8s • XKCD Pool
+
+
+Category: Mining
+Navigation menu
+Satoshi-NakamotoTalkPreferencesWatchlistContributionsLog out
+PageDiscussionReadView sourceView history
+More
+Search
+
+Main page
+Bitcoin FAQ
+Editing help
+Forums
+Chatrooms
+Recent changes
+Page index
+Tools
+What links here
+Related changes
+Special pages
+Printable version
+Permanent link
+Page information
+Sister projects
+Essays
+Source
+This page was last edited on 2021-06-23, at 01:02:44.
+Content is available under Creative Commons Attribution 3.0 unless otherwise noted.
+Privacy policyAbout Bitcoin WikiDisclaimers
